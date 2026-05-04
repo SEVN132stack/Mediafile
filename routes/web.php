4d718b5f27cb4ffdb95d2d27ffcf2ea08c45e1ec
SEVN132stack <troy.janssens.it@gmail.com>
2026-05-04 20:26:49 +0200

    d

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+// SPA catch-all - vang alles op behalve api routes
 Route::get('/{any}', function () {
     return view('app');
-})->where('any', '.*');
+})->where('any', '^(?!api).*$');
